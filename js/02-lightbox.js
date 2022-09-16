002935586788ed041dedf242399ab89ff4ced1dc
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-16 17:36:49 +0300

    add task 2 modul 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const addGalleryHtml = createGalleryHtml(galleryItems);
+
+function createGalleryHtml(items) {
+  return items
+    .map(
+      item =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.discription}"
+          />
+        </a>
+      </div>`
+    )
+    .join('');
+}
+
+galleryRef.innerHTML = addGalleryHtml;
+galleryRef.addEventListener('click', onImageclick);
+
+function onImageclick(e) {
+  blockedStandartAction(e);
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src = "${e.target.dataset.source}" width = "800" height = "600">`
+  );
+  instance.show();
+
+  galleryRef.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+function blockedStandartAction(e) {
+  e.preventDefault();
+}
