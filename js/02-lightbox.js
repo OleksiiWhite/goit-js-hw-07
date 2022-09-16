b1e68be9909af978866449bc49e5d0b9b06af11b
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-16 18:06:55 +0300

    fixed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,16 +10,15 @@ function createGalleryHtml(items) {
   return items
     .map(
       item =>
-        `<div class="gallery__item">
+        `<li class="gallery__item">
         <a class="gallery__link" href="${item.original}">
           <img
             class="gallery__image"
             src="${item.preview}"
-            data-source="${item.original}"
             alt="${item.discription}"
           />
         </a>
-      </div>`
+      </li>`
     )
     .join('');
 }
